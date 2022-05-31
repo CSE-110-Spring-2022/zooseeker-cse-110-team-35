6fd9bfed1969380b70dbc76bd392b52ccdbf60ca
David Hong <david.hong199@gmail.com>
2022-05-31 16:11:14 -0700

    fixed path generation test

--- app/src/test/java/edu/ucsd/cse110/zooseeker_team35/PathGenerationTest.java
+++ app/src/test/java/edu/ucsd/cse110/zooseeker_team35/PathGenerationTest.java
@@ -11,31 +11,43 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import edu.ucsd.cse110.zooseeker_team35.path_finding.IdentifiedWeightedEdge;
 import edu.ucsd.cse110.zooseeker_team35.path_finding.ZooData;
+import edu.ucsd.cse110.zooseeker_team35.path_finding.ZooInfoProvider;
 import edu.ucsd.cse110.zooseeker_team35.path_finding.ZooPathFinder;
 
 @RunWith(AndroidJUnit4.class)
 public class PathGenerationTest {
+    @Before
+    public void setup() {
+        Map<String, ZooData.VertexInfo> vertexInfo = ZooData.loadVertexInfoJSON(ApplicationProvider.getApplicationContext(), ZooInfoProvider.nodeInfoJSON);
+        Map<String, ZooData.EdgeInfo> edgeInfo = ZooData.loadEdgeInfoJSON(ApplicationProvider.getApplicationContext(), ZooInfoProvider.edgeInfoJSON);
+        ZooInfoProvider.setIdVertexMap(vertexInfo);
+        ZooInfoProvider.setIdEdgeMap(edgeInfo);
+    }
     @Test
     public void testPathCreation() {
         Context context = ApplicationProvider.getApplicationContext();
 
         String start = "entrance_exit_gate";
-        String solutionExhibitOne = "gators";
-        String solutionExhibitTwo = "gorillas";
-        String solutionExhibitThree = "elephant_odyssey";
+        String solutionExhibitOne = "flamingo";
+        String solutionExhibitTwo = "gorilla";
+        String solutionExhibitThree = "hippo";
         String end = "entrance_exit_gate";
 
-        Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON(context,"sample_zoo_graph2.json");
+        Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON(context,"sample_zoo_graph.json");
 
         ZooPathFinder zooPathFinder = new ZooPathFinder(g);
+
+
         List<String> targetExhibits = new LinkedList<>();
         targetExhibits.add(solutionExhibitTwo);
         targetExhibits.add(solutionExhibitThree);
@@ -53,10 +65,10 @@ public class PathGenerationTest {
         Context context = ApplicationProvider.getApplicationContext();
 
         String start = "entrance_exit_gate";
-        String solutionExhibitOne = "lions";
+        String solutionExhibitOne = "crocodile";
         String end = "entrance_exit_gate";
 
-        Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON(context,"sample_zoo_graph2.json");
+        Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON(context,"sample_zoo_graph.json");
 
         ZooPathFinder zooPathFinder = new ZooPathFinder(g);
         List<String> targetExhibits = new LinkedList<>();
